9ac14d6757af0a6b08fc30f69e82a6e384538e07
Chris <chris@gbp.co.uk>
2016-09-08 14:43:02 +0100

    Add company name to copyright in AssemblyInfo

--- source/GBMG.Monitoring/Properties/AssemblyInfo.cs
+++ source/GBMG.Monitoring/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Globe Business Media Group")]
 [assembly: AssemblyProduct("GBMG.Monitoring")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Globe Business Media Group 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
