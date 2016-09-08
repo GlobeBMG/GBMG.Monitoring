182ffc8b8123098425ad7610b7e86bd30bf54ada
Chris <chris@gbp.co.uk>
2016-09-08 12:59:27 +0100

    Set AssemblyInfo

--- source/GBMG.Monitoring/Properties/AssemblyInfo.cs
+++ source/GBMG.Monitoring/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GBMG.Monitoring")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("GBMG monitoring and telemetry abstraction")]
+[assembly: AssemblyDescription("A generic interface for sending custom metrics and telemetry to the configured monitoring provider")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Globe Business Media Group")]
 [assembly: AssemblyProduct("GBMG.Monitoring")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
